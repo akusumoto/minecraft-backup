32157b595452d4484ed800507a094994d178b414
kusumoto <gkusumoto@gmail.com>
2024-07-07 17:21:04 +0900

    1st version

--- minecraft-backup.py
+++ minecraft-backup.py
@@ -1,68 +1,114 @@
 #!/usr/bin/python
 
 import os 
-import zipfile
 import shutil
 import boto3
 import tempfile
-import datetime
+from datetime import datetime, timezone, timedelta
+import json
+import logging
 
-#BASEPATH = "/Users/akira.kusumoto/work/work/python/minecraft_backup/tmp"
-BASEPATH = r"C:\Users\kusumoto\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds"
-TARGET_DIRS = ["EvzZZWjpAgA=", "oMCHZhjmAAA="]
-S3_BACKET = "aks3-minecraft"
-N_BACKUP_GENERATION = 3
+PROGRAM_DIR = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(PROGRAM_DIR, "minecraft-backup.conf.json")
+""" ex) minecraft-backup.conf.json
+{
+    "aws_access_key_id": "*******",
+    "aws_secret_access_key": "******",
+    "region_name": "ap-northeast-1",
+    "world_data_path": "C:\\Users\\kusumoto\\AppData\\Local\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\LocalState\\games\\com.mojang\\minecraftWorlds",
+    "worlds": [
+        "EvzZZWjpAgA=",
+        "oMCHZhjmAAA="
+    ],
+    "s3_backet": "aks3-minecraft",
+    "n_backup_generation": "3"
+}
+"""
 
-def backup(s3_client: boto3.session.Session.client, target_dir: str):
+JST = timezone(timedelta(hours=+9), 'JST')
+
+logger = logging.getLogger("minecraft-backup")
+logging.basicConfig(filename=os.path.join(PROGRAM_DIR, "minecraft-backup.log"), 
+					encoding='utf-8', 
+					level=logging.INFO,
+					format='%(asctime)s - %(message)s', 
+					datefmt='%Y/%m/%d %H:%M:%S')
+
+
+def backup(s3_client: boto3.session.Session.client, s3_backet: str, world_data_path: str, target_dir: str):
 	with tempfile.TemporaryDirectory() as tmp_dir:
 		zip_file = target_dir + ".zip"
 		zip_path_noext = os.path.join(tmp_dir, target_dir)
 		zip_path = zip_path_noext + ".zip"
-		shutil.make_archive(zip_path_noext, format='zip', root_dir=BASEPATH, base_dir=target_dir)
+		shutil.make_archive(zip_path_noext, format='zip', root_dir=world_data_path, base_dir=target_dir)
 
 		# ex) zip_path = C:\Users\kusumoto\AppData\Local\Temp\tmp2rw0gxrq\oMCHZhjmAAA=.zip
 		#print(f"{zip_path} is {os.path.exists(zip_path)}")
 
-		upload(s3_client, zip_path)
+		upload(s3_client, s3_backet, zip_path)
 
-def upload(s3_client: boto3.session.Session.client, zip_path: str):
+def upload(s3_client: boto3.session.Session.client, s3_backet:str, zip_path: str):
 	zip_file = os.path.basename(zip_path)
 	zip_file_noext = os.path.splitext(zip_file)[0]
 	now =  datetime.datetime.now()
 	upload_filename = f"{zip_file_noext}_{now:%Y%m%d%H%M%S}.zip"
 
-	s3_client.upload_file(zip_path, S3_BACKET, upload_filename)
+	s3_client.upload_file(zip_path, s3_backet, upload_filename)
 	# ex) backuped oMCHZhjmAAA=_20240706.zip on aks3-minecraft
-	print(f"backuped {upload_filename} on {S3_BACKET}")
+	print(f"backuped {upload_filename} on {s3_backet}")
+	logger.info(f"backuped {upload_filename} on {s3_backet}")
 
-def delete_old_backup(s3_client: boto3.session.Session.client, target: str, generation: int):
-	objects = s3_client.list_objects(Bucket=S3_BACKET, Prefix=target)
-	backups = sorted([content for content in objects['Contents']], key=lambda x: x['LastModified'])
+def delete_old_backup(s3_client: boto3.session.Session.client, s3_backet: str, backups, world: str, generation: int):
+	#objects = s3_client.list_objects(Bucket=s3_backet, Prefix=world)
+	sorted_backups = sorted([backup for backup in backups if backup['Key'].startswith(world)], key=lambda x: x['LastModified'])
+	delete_backups = sorted_backups[:-generation]
+	for backup in delete_backups:
+		s3_client.delete_object(Bucket=s3_backet, Key=backup['Key'])
+		print(f"deleted old backup {backup['Key']}")
+		logger.info(f"deleted old backup {backup['Key']}")
+
+def load_config():
+	with open(CONFIG_PATH, "r") as f:
+		return json.load(f)
+
+def is_backuped(backups, world_data_path :str, world: str) -> bool:		
+	world_path = os.path.join(world_data_path, world)
+	world_mtime_timestamp = os.path.getmtime(world_path)
+	world_mtime = datetime.fromtimestamp(world_mtime_timestamp, JST)
+
+	for backup in backups:
+		if backup['Key'].startswith(world) and \
+		   backup['LastModified'] >= world_mtime:
+			return True
+	else:
+		return False
+
+def get_backups(s3_client: boto3.session.Session.client, s3_backet: str):
+	objects = s3_client.list_objects(Bucket=s3_backet)
+	return [content for content in objects['Contents'] if content['Key'].endswith('.zip')]
 	# ex) content
 	# [{'Key': 'EvzZZWjpAgA=_20240706.zip', 'LastModified': datetime.datetime(2024, 7, 6, 14, 36, 38, tzinfo=tzutc()), 'ETag': '"518734ef407ca6d95fa6d68b06f78ba5-17"', 'Size': 135211868, 'StorageClass': 'STANDARD', 'Owner': {'DisplayName': 'gkusumoto', 'ID': '091d46f8f3cc9802f81a28debf22ba8c386962b85305c6e0091a93336e5c097d'}}]
-	#print(backups)
-	delete_backups = backups[:-N_BACKUP_GENERATION]
-	for backup in delete_backups:
-		s3_client.delete_object(Bucket=S3_BACKET, Key=backup['Key'])
-		print(f"deleted old backup {backup['Key']}")
 
 if __name__ == '__main__':
+	config = load_config()
+	#print(config)
+
 	s3_client = boto3.client(
 		's3',
-		aws_access_key_id = '',
-		aws_secret_access_key = '',
-		region_name = 'ap-northeast-1'
+		aws_access_key_id = config['aws_access_key_id'],
+		aws_secret_access_key = config['aws_secret_access_key'],
+		region_name = config['region_name']
 	)
+
+	backups = get_backups(s3_client, config['s3_backet'])
 	
-	for target_dir in TARGET_DIRS:
-		#backup(s3_client, target_dir)
-		delete_old_backup(s3_client, target_dir, 3)
-
-import sys
-sys.exit(0)
-
-
-print(client.list_buckets())
-
-print(client.list_objects('ak3-minecraft'))
-
+	for world in config['worlds']:
+		if not is_backuped(backups, config['world_data_path'], world):
+			print(f"backup {world}")
+			logger.info(f"backup {world}")
+			backup(s3_client, config['s3_backet'], config['world_data_path'], world)
+		else:
+			print(f"{world} is already backuped")
+			logger.info(f"{world} is already backuped")
+		
+		delete_old_backup(s3_client, config['s3_backet'], backups, world, int(config['n_backup_generation']))
