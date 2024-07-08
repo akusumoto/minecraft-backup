c6e49abceea29ef5e70f5a6a5ef75942bf21d27a
Akira Kusumoto <gkusumoto@gmail.com>
2024-07-08 16:57:18 +0900

    error handling, notification message

--- minecraft-backup.py
+++ minecraft-backup.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-import os 
-import shutil
 import boto3
-import tempfile
+from botocore.exceptions import ClientError
 from datetime import datetime, timezone, timedelta
 import json
 import logging
+import os
+from plyer import notification
+import tempfile
+import shutil
+import sys
 
 PROGRAM_DIR = os.path.dirname(__file__)
 CONFIG_PATH = os.path.join(PROGRAM_DIR, "minecraft-backup.conf.json")
@@ -34,6 +37,15 @@ logging.basicConfig(filename=os.path.join(PROGRAM_DIR, "minecraft-backup.log"),
 					format='%(asctime)s - %(message)s', 
 					datefmt='%Y/%m/%d %H:%M:%S')
 
+def info(msg: str):
+	print(msg)
+	logger.info(msg)
+
+def error(msg: str, e: Exception):
+	print(f"{msg}: {e}")
+	logger.error(f"{msg}: {e}")
+	notification.notify(title="Backup Failed", message=msg)
+
 
 def backup(s3_client: boto3.session.Session.client, s3_backet: str, world_data_path: str, target_dir: str):
 	with tempfile.TemporaryDirectory() as tmp_dir:
@@ -45,27 +57,36 @@ def backup(s3_client: boto3.session.Session.client, s3_backet: str, world_data_p
 		# ex) zip_path = C:\Users\kusumoto\AppData\Local\Temp\tmp2rw0gxrq\oMCHZhjmAAA=.zip
 		#print(f"{zip_path} is {os.path.exists(zip_path)}")
 
-		upload(s3_client, s3_backet, zip_path)
+		return upload(s3_client, s3_backet, zip_path)
 
 def upload(s3_client: boto3.session.Session.client, s3_backet:str, zip_path: str):
 	zip_file = os.path.basename(zip_path)
 	zip_file_noext = os.path.splitext(zip_file)[0]
-	now =  datetime.datetime.now()
+	now =  datetime.now(JST)
 	upload_filename = f"{zip_file_noext}_{now:%Y%m%d%H%M%S}.zip"
 
-	s3_client.upload_file(zip_path, s3_backet, upload_filename)
+	try:
+		s3_client.upload_file(zip_path, s3_backet, upload_filename)
+	except ClientException as e:
+		error(f"Failed to backup world data {upload_filename} to S3 backet {s3_backet}",e)
+		raise e
+
 	# ex) backuped oMCHZhjmAAA=_20240706.zip on aks3-minecraft
-	print(f"backuped {upload_filename} on {s3_backet}")
-	logger.info(f"backuped {upload_filename} on {s3_backet}")
+	info(f"backuped {upload_filename} to {s3_backet}")
+
+	return {"Key":upload_filename, "LastModified": now}
 
 def delete_old_backup(s3_client: boto3.session.Session.client, s3_backet: str, backups, world: str, generation: int):
 	#objects = s3_client.list_objects(Bucket=s3_backet, Prefix=world)
 	sorted_backups = sorted([backup for backup in backups if backup['Key'].startswith(world)], key=lambda x: x['LastModified'])
 	delete_backups = sorted_backups[:-generation]
+
 	for backup in delete_backups:
-		s3_client.delete_object(Bucket=s3_backet, Key=backup['Key'])
-		print(f"deleted old backup {backup['Key']}")
-		logger.info(f"deleted old backup {backup['Key']}")
+		try:
+			s3_client.delete_object(Bucket=s3_backet, Key=backup['Key'])
+			info(f"deleted old backup {backup['Key']}")
+		except ClientException as e:
+			error(f"Failed to delete old backup world data {backup['Key']} on S3 backet {s3_backet}", e)
 
 def load_config():
 	with open(CONFIG_PATH, "r") as f:
@@ -90,8 +111,12 @@ def get_backups(s3_client: boto3.session.Session.client, s3_backet: str):
 	# [{'Key': 'EvzZZWjpAgA=_20240706.zip', 'LastModified': datetime.datetime(2024, 7, 6, 14, 36, 38, tzinfo=tzutc()), 'ETag': '"518734ef407ca6d95fa6d68b06f78ba5-17"', 'Size': 135211868, 'StorageClass': 'STANDARD', 'Owner': {'DisplayName': 'gkusumoto', 'ID': '091d46f8f3cc9802f81a28debf22ba8c386962b85305c6e0091a93336e5c097d'}}]
 
 if __name__ == '__main__':
-	config = load_config()
-	#print(config)
+	try:
+		config = load_config()
+		#print(config)
+	except Exception as e:
+		error(f"Failed to load config {CONFIG_PATH}", e)
+		sys.exit(1)
 
 	s3_client = boto3.client(
 		's3',
@@ -100,15 +125,26 @@ if __name__ == '__main__':
 		region_name = config['region_name']
 	)
 
-	backups = get_backups(s3_client, config['s3_backet'])
+	try:
+		backups = get_backups(s3_client, config['s3_backet'])
+	except Exception as e:
+		error(f"Failed to get object list from S3 bucket {config['s3_backet']}", e)
+		sys.exit(1)
 	
 	for world in config['worlds']:
-		if not is_backuped(backups, config['world_data_path'], world):
-			print(f"backup {world}")
-			logger.info(f"backup {world}")
-			backup(s3_client, config['s3_backet'], config['world_data_path'], world)
-		else:
-			print(f"{world} is already backuped")
-			logger.info(f"{world} is already backuped")
+		try:
+			if not is_backuped(backups, config['world_data_path'], world):
+				info(f"backup {world}")
+				obj = backup(s3_client, config['s3_backet'], config['world_data_path'], world)
+				backups.append(obj)
+			else:
+				info(f"{world} is already backuped")
 		
-		delete_old_backup(s3_client, config['s3_backet'], backups, world, int(config['n_backup_generation']))
+			delete_old_backup(s3_client, config['s3_backet'], backups, world, int(config['n_backup_generation']))
+
+		except OSError as e:
+			error(f"Failed to access world data {world}", e)
+		except ClientError as e:
+			# already output log in each function
+			pass
+
